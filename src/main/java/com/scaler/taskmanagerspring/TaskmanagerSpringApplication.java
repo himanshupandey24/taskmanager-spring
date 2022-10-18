e454fd2f0b7d0da2d4b4f3e5f2ad78e259224592
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-10-18 17:56:39 +0530

    basic mvc based project - taskmanager

---
+++ src/main/java/com/scaler/taskmanagerspring/TaskmanagerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.taskmanagerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/taskmanagerspring/TaskmanagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.taskmanagerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
